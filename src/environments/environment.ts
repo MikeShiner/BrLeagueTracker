12019bda22903d731c5748db0c0f11418c11b6c9
Mike Shiner <michael.shiner@siemens.com>
2020-10-17 21:59:05 +0100

    captain update

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -6,10 +6,13 @@ export const environment = {
   production: false,
   apiKey: '4eddc983-a606-4b87-bebd-53bc71fba426',
   proxyUrl: 'http://localhost:8080',
-  startTime: '2020-10-14T16:30:00Z',
+  startTime: '2020-10-15T11:30:00Z',
   captains: [
     { platform: 'atvi', id: 'Warscyther', teamName: 'Brothers In Arms' },
-    { platform: 'psn', id: 'TAdams944', teamName: 'Slavtastic' },
+    { platform: 'psn', id: 'TAdams944', teamName: 'Callout LichVegas' },
+    { platform: 'psn', id: 'durrant1993', teamName: 'CoD and Chips' },
+    { platform: 'psn', id: 'TinyEggMan', teamName: 'Ultra Legends Pheonix Rebirth' },
+    { platform: 'atvi', id: 'Bastable%237922822', teamName: 'Balltrick' },
   ],
 };
 

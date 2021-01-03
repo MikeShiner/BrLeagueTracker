13f11840566586ce99419d94f036465d9cb9e691
Mike Shiner <michael.shiner@siemens.com>
2021-01-03 01:44:05 +0000

    stash

--- warzone-ui/src/app/pages/leaderboard/leaderboard.component.ts
+++ warzone-ui/src/app/pages/leaderboard/leaderboard.component.ts
@@ -7,11 +7,18 @@ import { TrackerService } from "src/app/services/tracker.service";
   styleUrls: ["./leaderboard.component.scss"],
 })
 export class LeaderboardComponent implements OnInit {
+  tableType = "damage";
+  data;
   constructor(public trackerService: TrackerService) {}
 
   fakeArray(length: number) {
     return new Array(length);
   }
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.trackerService.killboard$.subscribe((board) => {
+      board.sort((a, b) => a[this.tableType] - b[this.tableType] * -1);
+      this.data = board;
+    });
+  }
 }
